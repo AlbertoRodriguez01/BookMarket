6fc24d4b332d3805e1e4a4360b1fcc8268050763
AlbertoRodriguez01 <37781005+AlbertoRodriguez01@users.noreply.github.com>
2024-05-19 23:55:46 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,84 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Navigation from './navigation/Navigation';
+import { LogBox } from 'react-native'
 
 
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Login from './screens/Login';
-import Home from './screens/Home';
-import Libros from './screens/Libros';
-import Comics from './screens/Comics';
-import Mostrar from './screens/Mostrar';
-import MostrarComic from './screens/MostrarComic';
-
+LogBox.ignoreAllLogs()
 
 export default function App() {
-
-  const Stack = createStackNavigator();
-
-  function MyStack() {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}
-          options={{
-            title: "LOGIN",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-        <Stack.Screen name="Home" component={Home}
-          options={{
-            title: "HOME",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-        <Stack.Screen name="Libros" component={Libros}
-          options={{
-            title: "Libros",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-        <Stack.Screen name="Comics" component={Comics}
-          options={{
-            title: "Comics",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-          <Stack.Screen name="Mostrar" component={Mostrar}
-          options={{
-            title: "Mostrar",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-          <Stack.Screen name="MostrarComic" component={MostrarComic}
-          options={{
-            title: "Mostrar",
-            headerTintColor: 'white',
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: "#36ADFC" },
-          }} />
-      </Stack.Navigator>
-      
-    );
-  }
-
-
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <Navigation/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
